f49e7dfce36a7ac6bdd48819b1da2826c3abe48b
cekongsb <806138244@qq.com>
2019-05-22 11:22:05 +0800

    modify pubkey_address_256 in MainNet&TestNet

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -548,9 +548,9 @@ public:
             DevFundSettings("RAJ3GeQ2UsCrqpPSS36pcDPnk2X4Ydi9kS", 10, 60));
 
 
-        base58Prefixes[PUBKEY_ADDRESS]     = {0x46}; // V
+        base58Prefixes[PUBKEY_ADDRESS]     = {0x47}; // V
         base58Prefixes[SCRIPT_ADDRESS]     = {0x3c};
-        base58Prefixes[PUBKEY_ADDRESS_256] = {0x39};
+        base58Prefixes[PUBKEY_ADDRESS_256] = {0x48}; // V or W
         base58Prefixes[SCRIPT_ADDRESS_256] = {0x3d};
         base58Prefixes[SECRET_KEY]         = {0x6c};
         base58Prefixes[EXT_PUBLIC_KEY]     = {0x69, 0x6e, 0x82, 0xd1}; // PPAR
@@ -754,9 +754,9 @@ public:
         //vDevFundSettings.push_back(std::make_pair(0, DevFundSettings("rTvv9vsbu269mjYYEecPYinDG8Bt7D86qD", 10, 60)));
         vDevFundSettings.push_back(std::make_pair(0, DevFundSettings("rQApnBUWAmJ28PHzaFw9gPeqiDFBxPWK3B", 10, 100)));
 
-        base58Prefixes[PUBKEY_ADDRESS]     = {0x84}; // v
+        base58Prefixes[PUBKEY_ADDRESS]     = {0x85}; // v
         base58Prefixes[SCRIPT_ADDRESS]     = {0x7a}; // r
-        base58Prefixes[PUBKEY_ADDRESS_256] = {0x77}; // p or q
+        base58Prefixes[PUBKEY_ADDRESS_256] = {0x86}; // v or w
         base58Prefixes[SCRIPT_ADDRESS_256] = {0x7b};
         base58Prefixes[SECRET_KEY]         = {0x2e};
         base58Prefixes[EXT_PUBLIC_KEY]     = {0xe1, 0x42, 0x78, 0x00}; // ppar
